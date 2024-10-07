896e7932053c87d59045bcec3f952eb20422046e
Vrutik Patel <vrutikpatel2022@gmail.com>
2024-10-07 19:08:06 -0400

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -30,19 +31,62 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double ghostPosition = -100;
+  double zombiePosition = -100;
+
+  @override
+  void initState() {
+    super.initState();
+    startAnimation();
+  }
+
+  void startAnimation() {
+    Timer.periodic(const Duration(microseconds: 50), (timer) {
+      setState(() {
+        ghostPosition += 1;
+        zombiePosition += 1;
+
+        if (ghostPosition > MediaQuery.of(context).size.width) {
+          ghostPosition = -100;
+        }
+
+        if (zombiePosition > MediaQuery.of(context).size.width) {
+          zombiePosition = -100;
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/halloween.jpeg'),
-            fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/halloween.jpeg'),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-        ),
+          AnimatedPositioned(
+            left: ghostPosition,
+            top: 100,
+            duration: const Duration(microseconds: 50),
+            child: Image.asset('assets/images/ghost.png', width: 200),
+          ),
+
+          AnimatedPositioned(
+            left: zombiePosition,
+            top: 500,
+            duration: const Duration(microseconds: 50),
+            child: Image.asset('assets/images/zombie.png', width: 200),
+          ),
+        ],
       ),
     );
   }
